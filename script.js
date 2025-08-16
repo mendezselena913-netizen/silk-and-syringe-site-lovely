405804c76060f9ac4e23544d4eb782d95c4dfca5
mendezselena913-netizen <mendezselena913@gmail.com>
2025-08-16 11:00:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".booking-form");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Thank you! Your booking request has been submitted.");
+      form.reset();
+    });
+  }
+});
 
